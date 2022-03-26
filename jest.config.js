c14519e1bf970ae591afcde331e3208b3becffd6
Igor Souza <igor.souza.webmaster@gmail.com>
2022-03-26 02:31:39 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules', '/.next/'],
+  moduleDirectories: ['node_modules', 'src'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/*.spec.tsx',
+    '!src/**/*.stories.tsx',
+    '!src/**/_app.tsx',
+    '!src/pages/**/*',
+    '!src/**/_document.tsx',
+    '!src/styles/**.ts',
+  ],
+};
