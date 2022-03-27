0cf472334c6a5676edb2beacfb949d67cbdc6a23
Igor Souza <igor.souza.webmaster@gmail.com>
2022-03-27 20:53:26 -0300

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/clean/presentation/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+        serif: ['Roboto', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+        mono: ['Roboto', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+        display: ['Roboto', '-apple-system', 'BlinkMacSystemFont'],
+        body: ['Roboto', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
